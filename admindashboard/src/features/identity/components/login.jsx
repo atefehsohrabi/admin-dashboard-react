3a97b2b2071d2c6288b56995ef8c8c5f50dc7019
a.sohrabi <a.sohrabi@douran.com>
2026-06-13 11:52:15 +0330

    feat: add css and fonts

--- admindashboard/src/features/identity/components/login.jsx
+++ admindashboard/src/features/identity/components/login.jsx
@@ -1,4 +1,5 @@
-const Login = () =>{
+import logo from '@assets/images/logo.svg';
+const Login = () => {
     return(
         <>
         <div className="text-center mt-4">
@@ -9,7 +10,7 @@ const Login = () =>{
         </p>
         <p className="lead">
           قبلا ثبت نام نکرده اید؟
-          <Link to="/register" className="me-2">ثبت نام کنید </Link>
+
         </p>
       </div>
 
@@ -40,3 +41,4 @@ const Login = () =>{
       </>
     )
 }
+export default Login;
